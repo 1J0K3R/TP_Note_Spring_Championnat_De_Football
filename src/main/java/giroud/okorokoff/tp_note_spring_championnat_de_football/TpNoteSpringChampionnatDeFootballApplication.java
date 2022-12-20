9755648451ae2df9b568b29df9524ec9d6b510c5
Joshua GIROUD <giroud.joshua69@gmail.com>
2022-12-20 09:14:54 +0100

    Initialize project

---
+++ src/main/java/giroud/okorokoff/tp_note_spring_championnat_de_football/TpNoteSpringChampionnatDeFootballApplication.java
@@ -0,0 +1,13 @@
+package giroud.okorokoff.tp_note_spring_championnat_de_football;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpNoteSpringChampionnatDeFootballApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpNoteSpringChampionnatDeFootballApplication.class, args);
+    }
+
+}
---
+++ src/test/java/giroud/okorokoff/tp_note_spring_championnat_de_football/TpNoteSpringChampionnatDeFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package giroud.okorokoff.tp_note_spring_championnat_de_football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpNoteSpringChampionnatDeFootballApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
